4bfc8648ce9d40d142be3866c0f5e5277e6ec6d0
Nathan Rogers <nrogers@nrogers-macbookpro.roam.corp.google.com>
2016-08-16 11:35:19 -0400

    deleted .direnv folder

--- .direnv/python-3.5.1/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jonalligood/PythonCode/TIY/week6/forums/cms/.direnv/python-3.5.1/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
